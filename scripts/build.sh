21e79df7a56f8df5adaade5a89be8609460df695
Act <pavlik4a@gmail.com>
2026-08-07 18:03:32 +0300

    changed the location of the build directory (target)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-script_path="$(dirname "$(realpath "$0")")"
+TARGET_DIR_REAL="$(realpath "$CARGO_TARGET")"
 
 source build.conf
-
-CARGO_TARGET="$script_path/../target" 
 
 scripts/bootstrap.sh
 
 cd "$UPSTREAM_PATH"
 
-mkdir -p "$CARGO_TARGET"
-CARGO_TARGET_DIR="$CARGO_TARGET" cargo build --release
+mkdir -p "$TARGET_DIR_REAL"
+CARGO_TARGET_DIR="$TARGET_DIR_REAL" cargo build --release
